f9d7cab7dafd6db98e507f09cc618eafac12940f
ceyylanmustafa@gmail.com <ceyylanmustafa@gmail.com>
2023-04-05 22:40:00 +0300

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsMyDictionary
+{
+  class MyDictionary<Key,Value>
+  {
+        Key[] years;
+        Value[] teams;
+
+        public MyDictionary()
+        {
+            years = new Key[0];
+            teams = new Value[0];
+        }
+
+        public void Add(Key yıl , Value takım )
+        {
+            Key[] TempArray = years;
+            years = new Key[years.Length + 1];
+            for (int i = 0; i <years.Length ; i++)
+            {
+                years[i] = TempArray[i];
+            }
+            years[years.Length - 1] = yıl;
+
+
+
+            Value[] TempArrayy = teams;
+            teams = new Value[teams.Length + 1];
+            for (int x = 0; x <teams.Length ; x++)
+            {
+                teams[x] = TempArrayy[x];
+            }
+            teams[teams.Length- 1] = takım;
+
+            Console.WriteLine(years[years.Length-1]+" " + teams[teams.Length-1]);
+        }
+
+       
+        
+
+
+
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace GenericsMyDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string> takımlar = new MyDictionary<int,string> ();
+
+            takımlar.Add(1905, "Galatasaray");
+            takımlar.Add(1907, "Fenerbahçe");
+            takımlar.Add(1903, "Beşiktaş");
+            takımlar.Add(1899, "FC Barcelona");
+
+
+        }
+    }
+}
